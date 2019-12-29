bc9e79b7d47562816de649973c0e628cddec936b
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-29 00:15:13 +0000

    Automatically backed up by Learn

---
+++ lib/class_methods_module.rb
@@ -0,0 +1,16 @@
+module MetaDancing
+ 
+  def metadata
+    "This class produces objects that love to dance."
+  end
+end
+
+class Dancer
+  extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+end
+
+class Kid
+  extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+end
